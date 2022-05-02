04849a229d33d4c6a9135d35ba38d8e9280c2e05
samarth <munde.samarth28@gmail.com>
2022-05-03 00:01:15 +0530

    added

---
+++ clothin-web/src/App.js
@@ -0,0 +1,15 @@
+const App =() => {
+  return (
+    <div className = "categories-container">
+      <div className='background-image'/>
+      <div className = 'category-container'>
+        <div className = 'category-body-container'>
+          <h2>Hats</h2>
+          <p>Buy Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
