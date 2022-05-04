44a7edebe1fd811be4109be20cdb1482051036ad
samarth <munde.samarth28@gmail.com>
2022-05-04 17:31:45 +0530

    adding of navigation bar

--- clothin-web/src/App.js
+++ clothin-web/src/App.js
@@ -1,15 +1,21 @@
-const App =() => {
+import { Route, Routes } from "react-router-dom";
+
+import Home from "../src/Route/Home/home.components";
+import NavigationBar from "../src/Route/Navigation/navigation.components";
+
+const Shop = () => {
+  return <h1>This is shop component</h1>;
+};
+
+const App = () => {
   return (
-    <div className = "categories-container">
-      <div className='background-image'/>
-      <div className = 'category-container'>
-        <div className = 'category-body-container'>
-          <h2>Hats</h2>
-          <p>Buy Now</p>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<NavigationBar />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
